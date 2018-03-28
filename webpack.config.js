5b4a76a6d1abafd998bba7bf849106c6af04971e
HT <908811048@qq.com>
2018-03-29 00:31:26 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+const path = require('path');
+const entry = require('./config/entry');
+const output = require('./config/output');
+const plugins = require('./config/plugins');
+const modules = require('./config/module');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin =require('clean-webpack-plugin')
+
+module.exports ={
+    entry:"./src",
+   // output:output,
+    mode:"development",
+     devtool:"source-map",
+    module:modules,
+    plugins:[
+        new htmlWebpackPlugin({
+            filename:"index.html",
+            template: "src/index.html",
+             chunks:"main"
+        }),
+        new CleanWebpackPlugin('./dist/*.*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        })
+    
+    ],
+}
